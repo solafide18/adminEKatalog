794f02b02869b8761479f9b8e67c70cbfb3db592
Solfide <l17011636@pamapersada.net>
2019-11-19 15:11:31 +0700

    test

--- resources/views/kompmanajerial/index.blade.php
+++ resources/views/kompmanajerial/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="block-header">
     <h2>Kamus Kompetensi Core Value</h2>
-    <h5>Badan Ekonomi Kreatif</h5>
+    <h5>Badan Ekonomi Kreatif - Test Menu</h5>
 </div>
 <div class="row clearfix">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
