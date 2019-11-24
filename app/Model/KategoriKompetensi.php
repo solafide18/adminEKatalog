89a0502a1836c131db602bcea2f872a54c107de6
Narwanto <narwanto@nostratech.com>
2019-11-24 11:05:47 +0700

    add controller

--- app/Model/KategoriKompetensi.php
+++ app/Model/KategoriKompetensi.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class KategoriKompetensi extends Model
 {
-    //
+    protected $table = "kategori_kompotensis";
+
+    protected $fillable = ['code','description'];
 }
