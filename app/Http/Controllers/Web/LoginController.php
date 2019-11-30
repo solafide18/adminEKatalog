f2c396acaa017617d94af0b326fedac14de5cbbd
Narwanto <narwanto@nostratech.com>
2019-11-30 16:09:57 +0700

    fixing session expired in login page

--- app/Http/Controllers/Web/LoginController.php
+++ app/Http/Controllers/Web/LoginController.php
@@ -10,22 +10,17 @@ use GuzzleHttp\Client;
 
 class LoginController extends Controller
 {
-    public function index() {
-        $data['data'] = "";
-        $data['errorLogin'] = false;
-
-        return view('login/index', $data);
+    public function index(Request $request) {
+        $request->session()->flush('authenticated');
+        return view('login/index');
     }
 
     public function login(Request $request) {
         $pin = $request->input('pin');
         $pass = $request->input('pass');
-        $request->session()->flush('authenticated', 'always');
-
-        error_log($pin);
-        error_log($pass);
+        $request->session()->flush('authenticated');
         $client = new Client;
-        $res = $client->get('https://sadewa.bekraf.go.id/api/login?token=7va9dfnf9v7df9av8sd7f9',
+        $res = $client->get('https://sadewa.bekraf.go.id/api/login?',
         [
             'query' => [
                 'token'=> '7va9dfnf9v7df9av8sd7f9',
@@ -35,24 +30,18 @@ class LoginController extends Controller
         ]);
 
         $jsonResponse = json_decode($res->getBody(), true);
-        error_log('login?');
-        error_log($jsonResponse['data']['login']);
         $data['data'] = $jsonResponse['data'];
         $data['errorLogin'] = false;
-        //$errorLogin = '<script>alert("User Name or Password invalid")</script>';
 
         if ($jsonResponse['data']['login'] == 1) {
             $request->session()->push('authenticated', 'always');
             return redirect()->action('Web\HomeController@index');
         }   
         else {
-            error_log('masuk else error');
-            $msg = $jsonResponse['message'];
-
+            $request->session()->flush('authenticated');
+            $msg = "username or password invalid";
             return view('login/index',['msg'=>$msg]);
         } 
-        
     }
-
 
 }
