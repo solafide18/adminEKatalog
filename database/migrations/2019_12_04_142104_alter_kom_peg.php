642259bf72a8ed826938c367ab073a6c7ce09b3f
Narwanto <narwanto@nostratech.com>
2019-12-04 21:22:30 +0700

    add migration alter table | add information in kompetensi_pegawais

---
+++ database/migrations/2019_12_04_142104_alter_kom_peg.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterKomPeg extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('kompetensi_pegawais', function (Blueprint $table) {
+            $table->longText('information')->nulable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
